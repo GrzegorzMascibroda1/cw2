9a0c1e23168b84c7648baa13418b6e1e746a3500
GrzegorzMascibroda1 <s33341@pjwstk.edu.pl>
2025-03-13 09:50:52 +0100

    próba zadania 1( nie działa )

---
+++ zadanie1.java
@@ -0,0 +1,24 @@
+public class zadanie1 {
+    public static int[] filterEvenIndexOddValue(int[] arr) {
+        int ile = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 1 && i % 2 == 0) {
+                ile++;
+            }
+        }
+        int[] result = new int[ile];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 1 && i % 2 == 0) {
+                result[i] = arr[i];
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] tablica_1 = {1,2,3,4,5,6};
+        int[] wynik = filterEvenIndexOddValue(tablica_1);
+        for (int i = 0; i < wynik.length; i++) {
+            System.out.print(wynik[i] + " ");
+        }
+    }
+}
